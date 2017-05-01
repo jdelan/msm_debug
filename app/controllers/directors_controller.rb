ace1f5f136917036fead3cd9a04c95057055e5a1
Jesse Delan <jesse.delan@users.noreply.github.com>
2017-05-01 14:20:23 -0500

    Completed 100% fixed

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,13 +1,16 @@
 class DirectorsController < ApplicationController
   def index
-    directors = Director.all
+    @directors = Director.all
+    render("/directors/index.html.erb")
   end
 
   def show
     @director = Director.find(params[:id])
+    render("/directors/show_details.html.erb")
   end
 
-  def new
+  def new_form
+    render("/directors/new_form.html.erb")
   end
 
   def create_row
@@ -19,24 +22,25 @@ class DirectorsController < ApplicationController
 
     @director.save
 
-    render("show")
+    render("show_details")
   end
 
   def edit_form
     @director = Director.find(params[:id])
+    render("directors/edit_form.html.erb")
   end
 
   def update_row
-    @director = Director.find(params[:id])
+    p = Director.find(params[:id])
 
-    @director.dob = params[":dob"]
-    @director.name = params[":name"]
-    @director.bio = params[":bio"]
-    @director.image_url = params[":image_url"]
+    p.dob = params[:the_dob]
+    p.name = params[:the_name]
+    p.bio = params[:the_bio]
+    p.image_url = params[:the_image_url]
 
-    @director.save
+    p.save
 
-    render("show")
+    redirect_to("/directors/#{p.id}")
   end
 
   def destroy
